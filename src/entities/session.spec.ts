74485ccf43c5ef4300d939da270bd6c6bc236020
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2022-08-28 13:07:20 +0200

    feat: add id to sessions

--- src/entities/session.spec.ts
+++ src/entities/session.spec.ts
@@ -1,34 +1,21 @@
-import {describe, it, expect, test} from 'vitest'
+import {describe, it, expect} from 'vitest'
 import Session from './session'
 
-describe('Session tests', () => {
-  it("should initialize successfully", () => {
-    // Given
+describe('session', () => {
     const startBPM = 20;
     const stopBPM = 100;
     const increaseBPM = 20;
-
-    // When
     const session = new Session(startBPM, stopBPM, increaseBPM);
 
-    // Then
+  it("should initialize successfully", () => {
     expect(session.startBPM).toBe(startBPM);
     expect(session.stopBPM).toBe(stopBPM);
     expect(session.increaseBPM).toBe(increaseBPM);
+    expect(session.id).toBeTypeOf("string");
   });
 
-
   it("should return the correct number of series for a session", () => {
-    // Given
-    const startBPM = 20;
-    const stopBPM = 100;
-    const increaseBPM = 20;
-    const session = new Session(startBPM, stopBPM, increaseBPM);
-
-    // When
     const numSeries = session.NumSeries();
-
-    // Then
     expect(numSeries).toBe(5);
   });
 })
--- src/entities/session.ts
+++ src/entities/session.ts
@@ -1,9 +1,13 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export default class Session {
+  id: string;
   startBPM: number;
   stopBPM: number;
   increaseBPM: number;
 
   constructor(startBPM :number, stopBPM: number, increaseBPM: number) {
+    this.id = uuidv4()
     this.startBPM = startBPM;
     this.stopBPM = stopBPM;
     this.increaseBPM = increaseBPM;
